a126e8fb64181a58a6615aa9936017317ab3d1c3
xuja <jina.koh@gmail.com>
2022-09-01 15:13:15 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -1,6 +1,7 @@
 module "network" {
   source  = "app.terraform.io/xuja/network/azurerm"
   version = "3.5.0"
+ resource_group_name = azurerm_resource_group.example.name
   # insert required variables here
 }
 
